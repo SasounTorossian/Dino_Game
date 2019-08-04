f90bb3f311e38410ba288b7fb2f5836857e4f1e7
SasounTorossian <46853885+SasounTorossian@users.noreply.github.com>
2019-08-04 19:20:59 +0100

    Basic game skeleton

---
+++ play.py
@@ -0,0 +1,233 @@
+import pygame
+import random
+import time
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GRAY = (159, 163, 168)
+
+pygame.init()
+
+# Set the width and height of the screen [width, height]
+size = (600, 300)
+screen = pygame.display.set_mode(size)
+
+# Load the fonts
+font_40 = pygame.font.SysFont("Arial", 40, True, False)
+font_30 = pygame.font.SysFont("Arial", 30, True, False)
+text_title = font_40.render("Dino Jump", True, BLACK)
+text_ins = font_30.render("Click to Play!", True, BLACK)
+pygame.display.set_caption("Dino Jump")
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# Loop until the user clicks the close button.
+done = False
+
+collision = True
+
+# Store the score
+score = 0
+
+
+# # Store speeding up mechanic
+# cactus_speed = [4, 9]
+# score_speed_threshold = 50
+
+
+# class Dinosaur:
+#     def __init__(self, x=0, y=0, dx=4, dy=4, width=40, height=40, color=RED):
+#         self.image = ""
+#         self.x = x
+#         self.y = y
+#         self.dx = dx
+#         self.dy = dy
+#         self.width = width
+#         self.height = height
+#         self.color = color
+#
+#     def load_image(self, img):
+#         self.image = pygame.image.load(img).convert()
+#         self.image.set_colorkey(BLACK)
+#
+#     def draw_image(self):
+#         screen.blit(self.image, [self.x, self.y])
+#
+#     def move_x(self):
+#         self.x += self.dx
+#
+#     def move_y(self):
+#         self.y += self.dy
+#
+#     def draw_rect(self):
+#         pygame.draw.rect(screen, self.color, [self.x, self.y, self.width, self.height], 0)
+#
+#     def check_out_of_screen(self):
+#         if self.x + self.width > 400 or self.x < 0:
+#             self.x -= self.dx
+
+
+class Cactus:
+    def __init__(self, x, y, dx, dy, width, height, color):
+        self.image = ""
+        self.x = x
+        self.y = y
+        self.dx = dx
+        self.dy = dy
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def load_image(self, img):
+        self.image = pygame.image.load(img).convert()
+        self.image.set_colorkey(BLACK)
+
+    def draw_image(self):
+        screen.blit(self.image, [self.x, self.y])
+
+    def move_x(self):
+        self.x += self.dx
+
+    def move_y(self):
+        self.y += self.dy
+
+    def draw_rect(self):
+        pygame.draw.rect(screen, self.color, [self.x, self.y, self.width, self.height], 0)
+
+    def check_out_of_screen(self):
+        if self.x + self.width > 400 or self.x < 0:
+            self.x -= self.dx
+
+
+# def check_collision(player_x, player_y, player_width, player_height, cac_x, cac_y, cac_width, cac_height):
+#     if (player_x + player_width > cac_x) and (player_x < cac_x + cac_width) and (player_y < cac_y + cac_height) and (
+#             player_y + player_height > cac_y):
+#         return True
+#     else:
+#         return False
+
+
+def draw_main_menu():
+    screen.blit(text_title, [size[0] / 2 - 106, size[1] / 2 - 100])
+    score_text = font_40.render("Score: " + str(score), True, BLACK)
+    screen.blit(score_text, [size[0] / 2 - 70, size[1] / 2 - 30])
+    screen.blit(text_ins, [size[0] / 2 - 85, size[1] / 2 + 40])
+    pygame.display.flip()
+
+
+# Create a player object
+# player = Dinosaur(175, 475, 0, 0, 90, 50, GRAY)
+# player.load_image("player_mod.png")
+
+
+# Setup the enemy cactus
+cactus = []
+cactus_count = 3
+for i in range(cactus_count):
+    car = Cactus(600, 180, -4, 0, 30, 40, BLACK)
+    cactus.append(car)
+
+def set_cactus_position():
+    cactus[0].x = random.randrange(600, 650)
+    for i in range(1, cactus_count):
+        cactus[i].x = cactus[i - 1].x + random.randrange(30, 300)
+
+
+def reset_cactus_position():
+    cactus[0].x = random.randrange(600, 650)
+    for i in range(1, cactus_count):
+        cactus[i].x = cactus[i - 1].x + random.randrange(30, 300)
+
+
+def move_cactus():
+    for i in range(cactus_count):
+        cactus[i].draw_rect()
+        cactus[i].move_x()
+        if cactus[i].x < 0:
+            cactus[i].y = 180
+            cactus[i].x = random.randrange(600, 800)
+
+
+# -------- Main Program Loop -----------
+while not done:
+
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+        # Reset everything when the user starts the game.
+        if collision and (event.type == pygame.MOUSEBUTTONDOWN or event.type == pygame.KEYDOWN):
+            collision = False
+            set_cactus_position()
+            # for i in range(cactus_count):
+            #     cactus[i].y = 180
+            #     cactus[i].x = 600
+            # player.x = 175
+            # player.dx = 0
+            # player.dy = 0
+            # score = 0
+
+            pygame.mouse.set_visible(False)
+
+        # if not collision:
+        #     if event.type == pygame.KEYDOWN:
+        #         if event.key == pygame.K_UP or event.key == pygame.K_SPACE:
+        #             player.dx = 4
+        #
+        #     if event.type == pygame.KEYUP:
+        #         if event.key == pygame.K_UP or or event.key == pygame.K_SPACE:
+        #             player.dx = 0
+
+    # Screen-clearing code goes here
+    screen.fill(GRAY)
+
+    # Drawing code should go here
+    if not collision:
+        pygame.draw.line(screen, BLACK, (0, 200), (600, 200))
+
+        # player.draw_image()
+        # player.move_y()
+        # player.check_out_of_screen()
+
+        # cactus_onscreen = random.randint(1, 3)
+        # Generate cactus.
+        # for i in range(cactus_count):
+
+        # cactus[0].draw_rect()
+        # cactus[0].move_x()
+        # if cactus[0].x < 0:
+        #     cactus[0].y = 180
+        #     cactus[0].x = 600
+        #
+        # cactus[1].draw_rect()
+        # cactus[1].move_x()
+        # if cactus[1].x < 0:
+        #     cactus[1].y = 180
+        #     cactus[1].x = 600
+
+        move_cactus()
+
+        # # Check the collision of the player with the cactus
+        # for i in range(cactus_count):
+        #     if check_collision(player.x, player.y, player.width, player.height, cactus[i].x, cactus[i].y, cactus[i].width,
+        #                        cactus[i].height):
+        #         collision = True
+        #         pygame.mouse.set_visible(True)
+        #         break
+
+        # Draw the score.
+        txt_score = font_30.render("Score: " + str(score), True, WHITE)
+        screen.blit(txt_score, [15, 15])
+
+        pygame.display.flip()
+    else:
+        draw_main_menu()
+
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+
+# Close the window and quit.
+pygame.quit()
