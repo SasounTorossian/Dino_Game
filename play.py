8e354de2e55520861993dd994aad453f7376e120
SasounTorossian <46853885+SasounTorossian@users.noreply.github.com>
2020-04-21 19:30:43 +0100

    Added major gui functions into Gui class. Needs streamlining.

--- play.py
+++ play.py
@@ -9,33 +9,52 @@ import pygame
 # TODO: Expand screen size?
 # TODO: Open in center of screen?
 
-# Define some colors
-BLACK = (0, 0, 0)
-OFF_WHITE = (225, 225, 225)
-OFF_GRAY = (80, 80, 80)
-OFF_LIGHT_GRAY = (115, 115, 115)
 
 # Initialise game engine
 pygame.init()
 
-# Set the width and height of the screen [width, height]
-SIZE = (600, 300)
-screen = pygame.display.set_mode(SIZE)
 
-# 8-bit Font file path
-FONT_FILE_PATH = "C:\\Users\\Sasoun\\Documents\\Software Workspace\\PycharmProjects\\Dino_Game\\VCR_OSD_MONO_1.001.ttf"
+class Gui():
+    BLACK = (0, 0, 0)
+    OFF_WHITE = (225, 225, 225)
+    OFF_GRAY = (80, 80, 80)
+    OFF_LIGHT_GRAY = (115, 115, 115)
+    SIZE = (600, 300)
+    FONT_FILE_PATH = "C:\\Users\\Sasoun\\Documents\\Software Workspace\\PycharmProjects\\Dino_Game\\VCR_OSD_MONO_1.001.ttf"
 
-# Load the fonts
-font_50 = pygame.font.Font(FONT_FILE_PATH, 50)
-font_40 = pygame.font.Font(FONT_FILE_PATH, 40)
-font_30 = pygame.font.Font(FONT_FILE_PATH, 30)
-font_20 = pygame.font.Font(FONT_FILE_PATH, 20)
-font_10 = pygame.font.Font(FONT_FILE_PATH, 10)
-text_title = font_50.render("Dino Jump", True, BLACK)
-text_ins = font_30.render("Click to Play", True, BLACK)
-name_tag = font_10.render("Sasoun Torossian", True, BLACK)
-pygame.display.set_caption("Dino Jump")
+    def __init__(self):
+        self.screen = pygame.display.set_mode(self.SIZE)
+        self.font_50 = pygame.font.Font(self.FONT_FILE_PATH, 50)
+        self.font_40 = pygame.font.Font(self.FONT_FILE_PATH, 40)
+        self.font_30 = pygame.font.Font(self.FONT_FILE_PATH, 30)
+        self.font_20 = pygame.font.Font(self.FONT_FILE_PATH, 20)
+        self.font_10 = pygame.font.Font(self.FONT_FILE_PATH, 10)
+        self.text_title = self.font_50.render("Dino Jump", True, self.BLACK)
+        self.text_ins = self.font_30.render("Click to Play", True, self.BLACK)
+        self.name_tag = self.font_10.render("Sasoun Torossian", True, self.BLACK)
+        pygame.display.set_caption("Dino Jump")
 
+    def draw_main_menu(self):
+        self.screen.blit(self.text_title, [self.SIZE[0] / 2 - 130, self.SIZE[1] / 2 - 100])
+        score_text = self.font_40.render("Score:" + str(score).zfill(5), True, self.BLACK)
+        self.screen.blit(score_text, [self.SIZE[0] / 2 - 135, self.SIZE[1] / 2 - 30])
+        self.screen.blit(self.text_ins, [self.SIZE[0] / 2 - 120, self.SIZE[1] / 2 + 40])
+        self.screen.blit(self.name_tag, [self.SIZE[0] / 2 - 290, self.SIZE[1] / 2 + 135])
+        pygame.display.flip()
+
+    def draw_score(self):
+        if not hi_score:
+            txt_score = self.font_20.render(str(score).zfill(5), True, self.OFF_GRAY)
+            self.screen.blit(txt_score, [530, 15])
+        else:
+            # Two separate renders for colour contrast
+            txt_hi_score = self.font_20.render("HI " + str(hi_score).zfill(5), True, self.OFF_LIGHT_GRAY)
+            self.screen.blit(txt_hi_score, [430, 15])
+            txt_score = self.font_20.render(str(score).zfill(5), True, self.OFF_GRAY)
+            self.screen.blit(txt_score, [530, 15])
+
+
+gui = Gui()
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
 
@@ -52,28 +71,6 @@ spawnCactus = pygame.USEREVENT+2
 moveCactus = pygame.USEREVENT+3
 
 
-# TODO: Put all into GUI class. Different screens will inherit?
-def draw_main_menu():
-    screen.blit(text_title, [SIZE[0] / 2 - 130, SIZE[1] / 2 - 100])
-    score_text = font_40.render("Score:" + str(score).zfill(5), True, BLACK)
-    screen.blit(score_text, [SIZE[0] / 2 - 135, SIZE[1] / 2 - 30])
-    screen.blit(text_ins, [SIZE[0] / 2 - 120, SIZE[1] / 2 + 40])
-    screen.blit(name_tag, [SIZE[0] / 2 - 290, SIZE[1] / 2 + 135])
-    pygame.display.flip()
-
-
-def draw_score():
-    if not hi_score:
-        txt_score = font_20.render(str(score).zfill(5), True, OFF_GRAY)
-        screen.blit(txt_score, [530, 15])
-    else:
-        # Two separate renders for colour contrast
-        txt_hi_score = font_20.render("HI " + str(hi_score).zfill(5), True, OFF_LIGHT_GRAY)
-        screen.blit(txt_hi_score, [430, 15])
-        txt_score = font_20.render(str(score).zfill(5), True, OFF_GRAY)
-        screen.blit(txt_score, [530, 15])
-
-
 class Object():
     def __init__(self, x, y, dx, dy, width, height, image):
         self.image = pygame.image.load(image).convert_alpha()
@@ -88,7 +85,7 @@ class Object():
         self.image = pygame.image.load(image).convert_alpha()
 
     def draw_image(self):
-        screen.blit(self.image, [self.x, self.y])
+        gui.screen.blit(self.image, [self.x, self.y])
 
     def move_x(self):
         self.x += self.dx
@@ -103,7 +100,7 @@ class Object():
 
     # Draw rectangle instead of image. Used for testing.
     def draw_rect(self):
-        pygame.draw.rect(screen, [self.x, self.y, self.width, self.height], 0)
+        pygame.draw.rect(Gui.screen, [self.x, self.y, self.width, self.height], 0)
 
     @classmethod
     def check_collision(cls, dino, cactus_arr):
@@ -219,7 +216,7 @@ while True:
             pygame.time.set_timer(spawnCactus, Cactus.spawn_time[random.randint(0, len(Cactus.spawn_time)-1)])
 
     # Screen-clearing code goes here
-    screen.fill(OFF_WHITE)
+    gui.screen.fill(gui.OFF_WHITE)
 
     # Drawing code should go here
     if collision:
@@ -230,10 +227,10 @@ while True:
         if score > hi_score:  # Calculate hi score
             hi_score = score
 
-        draw_main_menu()
+        gui.draw_main_menu()
     else:
         event_lock = False
-        pygame.draw.line(screen, BLACK, (0, 200), (600, 200))  # Draw ground line.
+        pygame.draw.line(gui.screen, gui.BLACK, (0, 200), (600, 200))  # Draw ground line.
 
         dino.dino_move()
         Cactus.move_cactus(cactus_arr)
@@ -241,7 +238,7 @@ while True:
         collision = Object.check_collision(dino, cactus_arr)
 
         # Draw the score.
-        draw_score()
+        gui.draw_score()
 
         # Updates entire surface display
         pygame.display.flip()
